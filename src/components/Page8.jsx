79154d13293d50448c6bebae671d816088989e0d
ashmitaa199 <129546991+ashmitaa199@users.noreply.github.com>
2024-06-06 01:57:10 +0530

    vv

--- src/components/Page8.jsx
+++ src/components/Page8.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import wrings from "../assets/wrings.png"
-import iphone from "../assets/iphone3.png"
+import iphone from "../assets/iPhone3.png"
 import bg1 from "../assets/bg1.png"
 export default function Page8(){
     return(
